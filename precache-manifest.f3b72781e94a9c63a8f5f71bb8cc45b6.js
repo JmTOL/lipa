9e2e4253796befea0223562e8d335e314a4cd817
Jm Tolentino <tolentino.johnmatthew@gmail.com>
2020-10-14 22:43:51 +0800

    Updates

---
+++ precache-manifest.f3b72781e94a9c63a8f5f71bb8cc45b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cc0b0c8aec7e7fe32ea0cf9fb19167",
+    "url": "/lipa/index.html"
+  },
+  {
+    "revision": "921cc69233c07e0977bb",
+    "url": "/lipa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a45e0334d184570f1a4a",
+    "url": "/lipa/static/js/2.bd352239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lipa/static/js/2.bd352239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921cc69233c07e0977bb",
+    "url": "/lipa/static/js/main.16312f82.chunk.js"
+  },
+  {
+    "revision": "b5dfe7fb68c58932696a",
+    "url": "/lipa/static/js/runtime-main.c4f275df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lipa/static/media/logo.5d5d9eef.svg"
+  }
+]);
